d3bd5f4749a94ac49b039e7e7d777b00c375be5b
Matthew Wester <MatthewWester@live.com>
2019-11-23 00:22:29 -0700

    Added initial html, css, and javascript placeholder pages. Password generator not yet with working javascript.

---
+++ scripts.js
@@ -0,0 +1,25 @@
+let newPassword = "user1234";
+
+const allCharacters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!#$%^&*+=-@_".split('');
+
+function add2() {
+    return 3;
+}
+
+function makePassword() {
+        document.getElementById("password").innerHTML = add2();
+}
+
+console.log(passwordLength);
+// Math.floor(Math.random() * 10) gives number between 0 and 1
+/* <script type="text/javascript">  
+function getcube(){  
+var number=document.getElementById("number").value;  
+alert(number*number*number);  
+}  
+</script>  
+<form>  
+Enter No:<input type="text" id="number" name="number"/><br/>  
+<input type="button" value="cube" onclick="getcube()"/>  
+</form>  
+ */
