6534c11bb6d3956b7e916f142c4fea16215e2dc3
Matthew Wester <MatthewWester@live.com>
2019-11-23 13:48:36 -0700

    Deleted some unnecessary lines no longer being used

--- scripts.js
+++ scripts.js
@@ -1,19 +1,12 @@
-let newPassword = "user1234";
 var characterLength = document.getElementById("characterlength").value;
-
 const allCharacters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!#$%^&*+=-@_".split('');
 
 function createPassword() {
     return document.getElementById("characterlength").value;
 }
 
-function characterLengthChanged() {
-    let characterLength = document.getElementById("characterlength");
-    console.log(characterLength);
-}
-
 function showPassword() {
-        document.getElementById("password").innerHTML = createPassword();
+        document.getElementById("password").innerHTML = document.getElementById("characterlength").value;
 }
 
 // Math.floor(Math.random() * 10) gives number between 0 and 1
