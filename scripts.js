4948ee41e39c08f3aa3daf0f379bf5de06dd9dca
Matthew Wester <MatthewWester@live.com>
2019-11-23 23:32:30 -0700

    Generate Password button now creates one of each requested character type but is not working beyond that.

--- scripts.js
+++ scripts.js
@@ -1,14 +1,62 @@
-const allCharacters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!#$%^&*+=-@_".split('');
+const special = ["+", "-", "&&", "||", "!", "(", ")", "{", "}", "[", "]", "^",
+"~", "*", "?", ":", "\"", "\\"];
+const lower = "abcdefghijklmnopqrstuvwxyz".split('');
+const uppity = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split('');
+const numeric = "0123456789".split('');
+// let password = [];
+// let passwordChoice = [];
 
+//Grabs a random element from an array
+function getRandomFromArray(arr) {
+    var randomNumber = (Math.floor((Math.random() * arr.length)));
+    return arr[randomNumber];
+}
+
+//Is initialized when you hit the 'generate password' button. It does one thing: makes a password to put in the password text box.
 function generatePassword() {
-    if (document.getElementById("characterlength").value < 8 || document.getElementById("characterlength").value > 128) {
+    let charlength = Number(document.getElementById("characterlength").value);
+    let password = [];
+    let passwordChoice = [];
+    if (charlength < 8 || charlength > 128) {
         alert("Please enter a viable number for password character length.");
     }
-    if (document.getElementById("chtype1").checked === true) {
-        console.log("It worked!");
+    if (typeof charlength !== "number") {
+        alert("Please enter a number for password character length, please.");
     }
-    document.getElementById("password").innerHTML = document.getElementById("characterlength").value;
+    if (document.getElementById("chtype1").checked === true) {
+        password.push(getRandomFromArray(uppity));
+        console.log(getRandomFromArray(uppity));
+        charlength --;
+        passwordChoice.concat(uppity);
+    }
+    console.log(passwordChoice);
+    if (document.getElementById("chtype2").checked === true) {
+        password.push(getRandomFromArray(numeric));
+        charlength --;
+        passwordChoice.concat(numeric);
+        console.log(passwordChoice);
+    }
+    console.log(passwordChoice);
+    if (document.getElementById("chtype3").checked === true) {
+        password.push(getRandomFromArray(lower));
+        charlength --;
+        passwordChoice.concat(lower);
+        console.log(passwordChoice);
+    }
+    if (document.getElementById("chtype4").checked === true) {
+        password.push(getRandomFromArray(special));
+        charlength --;
+        console.log(special);
+        console.log(passwordChoice);
+    }
+    while (charlength > 0) {
+        charlength --;
+        password.push("The next is " + getRandomFromArray(passwordChoice));
+    }
+    console.log(passwordChoice);
+    document.getElementById("password").innerHTML = password.toString();
 }
+
 
 function scramblePassword() {
     if (document.getElementById("password").value === "") {
@@ -17,25 +65,11 @@ function scramblePassword() {
         scrambled = document.getElementById("password").value.split('');
         console.log(scrambled);
         leng = scrambled.length;
-
+//does not work correctly below
         for (var i = leng - 1; i > 0; i--) {
             var j = Math.floor(Math.random() * (i + 1));
             var k = scrambled[i];
             scrambled[j] = k;
         }
-        console.log(scrambled.join(""));
     }
 }
-
-// Math.floor(Math.random() * 10) gives number between 0 and 1
-/* <script type="text/javascript">
-function getcube(){
-var number=document.getElementById("number").value;
-alert(number*number*number);
-}
-</script>
-<form>
-Enter No:<input type="text" id="number" name="number"/><br/>
-<input type="button" value="cube" onclick="getcube()"/>
-</form>
- */
