7d115b18d48afb87ade10234fcbe7e0be4a44961
Matthew Wester <MatthewWester@live.com>
2019-11-23 18:38:48 -0700

    Error messages added for a couple edge cases. Beginning attempt to scramble an array.

--- scripts.js
+++ scripts.js
@@ -1,23 +1,41 @@
-var characterLength = document.getElementById("characterlength").value;
 const allCharacters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!#$%^&*+=-@_".split('');
 
-function createPassword() {
-    return document.getElementById("characterlength").value;
+function generatePassword() {
+    if (document.getElementById("characterlength").value < 8 || document.getElementById("characterlength").value > 128) {
+        alert("Please enter a viable number for password character length.");
+    }
+    if (document.getElementById("chtype1").checked === true) {
+        console.log("It worked!");
+    }
+    document.getElementById("password").innerHTML = document.getElementById("characterlength").value;
 }
 
-function showPassword() {
-        document.getElementById("password").innerHTML = document.getElementById("characterlength").value;
+function scramblePassword() {
+    if (document.getElementById("password").value === "") {
+        alert("There is no generated password to scramble yet!");
+    } else {
+        scrambled = document.getElementById("password").value.split('');
+        console.log(scrambled);
+        leng = scrambled.length;
+
+        for (var i = leng - 1; i > 0; i--) {
+            var j = Math.floor(Math.random() * (i + 1));
+            var k = scrambled[i];
+            scrambled[j] = k;
+        }
+        console.log(scrambled.join(""));
+    }
 }
 
 // Math.floor(Math.random() * 10) gives number between 0 and 1
-/* <script type="text/javascript">  
-function getcube(){  
-var number=document.getElementById("number").value;  
-alert(number*number*number);  
-}  
-</script>  
-<form>  
-Enter No:<input type="text" id="number" name="number"/><br/>  
-<input type="button" value="cube" onclick="getcube()"/>  
-</form>  
+/* <script type="text/javascript">
+function getcube(){
+var number=document.getElementById("number").value;
+alert(number*number*number);
+}
+</script>
+<form>
+Enter No:<input type="text" id="number" name="number"/><br/>
+<input type="button" value="cube" onclick="getcube()"/>
+</form>
  */
