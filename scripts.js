d30edff87377d436279460e0080a3943ce5a1e87
Matthew Wester <MatthewWester@live.com>
2019-11-24 17:30:39 -0700

    Woot woot! Generate Password and Scramble Password functions both work!

--- scripts.js
+++ scripts.js
@@ -1,75 +1,80 @@
-const special = ["+", "-", "&&", "||", "!", "(", ")", "{", "}", "[", "]", "^",
-"~", "*", "?", ":", "\"", "\\"];
+const special = ["+", "-", "&", "|", "!", "(", ")", "{", "}", "[", "]", "^",
+    "~", "*", "?", ":", "\"", "\\"];
 const lower = "abcdefghijklmnopqrstuvwxyz".split('');
 const uppity = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split('');
 const numeric = "0123456789".split('');
-// let password = [];
-// let passwordChoice = [];
 
-//Grabs a random element from an array
+//Grabs a random element from an array, a function used in below functions.
 function getRandomFromArray(arr) {
-    var randomNumber = (Math.floor((Math.random() * arr.length)));
+    var randomNumber = (Math.floor(Math.random() * arr.length));
     return arr[randomNumber];
+}
+
+//Adding array onto end of another array
+function arrayAdder(arr1, arr2) {
+    for (i = 0; i < arr2.length; i++) {
+        arr1.push(arr2[i]);
+    }
 }
 
 //Is initialized when you hit the 'generate password' button. It does one thing: makes a password to put in the password text box.
 function generatePassword() {
     let charlength = Number(document.getElementById("characterlength").value);
     let password = [];
-    let passwordChoice = [];
+    let passwordChoices = [];
+    // If inputted character length not right.
     if (charlength < 8 || charlength > 128) {
         alert("Please enter a viable number for password character length.");
+        return;
     }
-    if (typeof charlength !== "number") {
-        alert("Please enter a number for password character length, please.");
+    // If no boxes checked regarding characters. 
+    if (document.getElementById("chtype1").checked === false && document.getElementById("chtype2").checked === false && document.getElementById("chtype3").checked === false && document.getElementById("chtype4").checked === false) {
+        alert("Please check at least one box for desired character types");
+        return;
     }
+    // Below handles the user checking 1 to 4 of the boxes for character types.
     if (document.getElementById("chtype1").checked === true) {
         password.push(getRandomFromArray(uppity));
-        console.log(getRandomFromArray(uppity));
-        charlength --;
-        passwordChoice.concat(uppity);
+        charlength--;
+        arrayAdder(passwordChoices, uppity);
     }
-    console.log(passwordChoice);
     if (document.getElementById("chtype2").checked === true) {
         password.push(getRandomFromArray(numeric));
-        charlength --;
-        passwordChoice.concat(numeric);
-        console.log(passwordChoice);
+        charlength--;
+        arrayAdder(passwordChoices, numeric);
     }
-    console.log(passwordChoice);
     if (document.getElementById("chtype3").checked === true) {
         password.push(getRandomFromArray(lower));
-        charlength --;
-        passwordChoice.concat(lower);
-        console.log(passwordChoice);
+        charlength--;
+        arrayAdder(passwordChoices, lower);
     }
     if (document.getElementById("chtype4").checked === true) {
         password.push(getRandomFromArray(special));
-        charlength --;
-        console.log(special);
-        console.log(passwordChoice);
+        charlength--;
+        arrayAdder(passwordChoices, special);
     }
+    //fills out the rest of the password length knowing the user wants characters that are now within passwordChoice array
     while (charlength > 0) {
-        charlength --;
-        password.push("The next is " + getRandomFromArray(passwordChoice));
+        charlength--;
+        password.push(getRandomFromArray(passwordChoices));
     }
-    console.log(passwordChoice);
-    document.getElementById("password").innerHTML = password.toString();
+    document.getElementById("password").innerHTML = password.join("");
 }
 
 
 function scramblePassword() {
     if (document.getElementById("password").value === "") {
         alert("There is no generated password to scramble yet!");
+        return;
     } else {
-        scrambled = document.getElementById("password").value.split('');
-        console.log(scrambled);
+        scrambled = document.getElementById("password").value.split("");
         leng = scrambled.length;
-//does not work correctly below
-        for (var i = leng - 1; i > 0; i--) {
-            var j = Math.floor(Math.random() * (i + 1));
-            var k = scrambled[i];
-            scrambled[j] = k;
+        for (var b = leng - 1; b > 0; b--) {
+            var c = Math.floor(Math.random() * (b + 1));
+            var d = scrambled[b];
+            scrambled[b] = scrambled[c];
+            scrambled[c] = d;
+            document.getElementById("password").innerHTML = scrambled.join("");
         }
     }
 }
